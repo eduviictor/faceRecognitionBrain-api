ee24a0a307dd82038adbbad6c79711827b5ad34d
eduviictor <eduvictornobrega@gmail.com>
2019-01-24 15:29:29 -0300

    changing text response

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('Unable to join'));  
+    .catch(err => res.status(400).json('Unable to register'));  
 }
 
 module.exports = {
